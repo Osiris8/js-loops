8fd0c427b5ccff0ec0a275bd8d3ca1e451001c1f
Osiris8 <miganosiris8@gmaaail.com>
2022-11-12 22:58:11 +0100

    Do...While Statements

---
+++ part8.js
@@ -0,0 +1,14 @@
+// Write your code below
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++;
+  console.log(cupsAdded + " cup was added");
+} while (cupsAdded < cupsOfSugarNeeded);
+
+/* Result
+1 cup was added
+2 cup was added
+3 cup was added
+*/
