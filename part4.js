a54f6f29bd53aca0bd2d88765283e7c049ea8dde
Osiris8 <miganosiris8@gmaaail.com>
2022-11-12 22:33:55 +0100

    Looping through Arrays

---
+++ part4.js
@@ -0,0 +1,12 @@
+const vacationSpots = ["Bali", "Paris", "Tulum"];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log("I would love to visit " + vacationSpots[i]);
+}
+
+/*Result
+I would love to visit Bali
+I would love to visit Paris
+I would love to visit Tulum
+*/
